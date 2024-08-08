4e17d33833966ea7e495c21105229006de37942f
Pithiwit <wazadafa14@gmail.com>
2024-08-08 15:06:52 +0700

    ed1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,14 @@ export default function Home() {
         <div className="vstack gap-3">
           <div className="d-flex align-items-center gap-3">
             <img
-              src="/profileImages/handsome.jpg"
+              src="/profileImages/profile778.JPG"
               width="48"
               height="48"
               className="rounded-circle"
               style={{ objectFit: "cover" }}
             />
             <span className="fw-semibold fs-5 text-white">
-              Chayanin Suatap 650610560
+              Pithiwit Netkhamyuang 660610778
             </span>
           </div>
 
@@ -80,13 +80,52 @@ export default function Home() {
             </span>
             <br />
             <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
+          </div>
+        </div>
+        <div className="d-flex gap-2 my-2 ps-5">
+          <img
+            src="/profileImages/popcat.png"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              Cat Meme
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>ลิซ่าาาาาา</span>
             <div className="d-flex align-items-center gap-1">
               <img src="/like.svg" width={20}></img>
               <span style={{ color: "#B0B3B8" }}>2 คน</span>
             </div>
           </div>
         </div>
-
+        <div className="d-flex gap-2 my-2">
+          <img
+            src="/profileImages/charliebrown.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              Charlie Brown
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>บ้าไปแล้ว</span>
+            <div className="d-flex align-items-center gap-1">
+            </div>
+          </div>
+        </div>
         {/* map-loop render Comment component here */}
       </div>
     </div>
